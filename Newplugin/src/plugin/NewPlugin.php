7702c664a383d0c6972b9600edab1f1dca6e03ab
Camthal1on <yungtrappriceofbelair@gmail.com>
2015-06-08 08:41:24 -0400

    Update NewPlugin.php

--- Newplugin/src/plugin/NewPlugin.php
+++ Newplugin/src/plugin/NewPlugin.php
@@ -13,3 +13,22 @@ public function onEnable(){
 
 
 }
+$pl() = array($coal, $iron, $gold, $diamond)
+give $pl{
+   $coal rand(0,9);
+  $iron rand(0,9);
+  $gold rand(0,9);
+  $diamond rand(0,9);
+  
+}
+$lotto = array($coal, $iron, $gold, $diamond){
+ do {
+  $coal rand(0,9);
+  $iron rand(0,9);
+  $gold rand(0,9);
+  $diamond rand(0,9);
+ }
+}
+
+give 
+
