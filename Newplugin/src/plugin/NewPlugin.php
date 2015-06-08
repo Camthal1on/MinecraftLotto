aab2421a116e1dcfbd7e68fb3ccf8d7a9cf401d2
Camthal1on <yungtrappriceofbelair@gmail.com>
2015-06-08 09:31:35 -0400

    Update NewPlugin.php

--- Newplugin/src/plugin/NewPlugin.php
+++ Newplugin/src/plugin/NewPlugin.php
@@ -1,20 +1,29 @@
 <?php
-namespace plugin;
+namespace MinecraftLotto;
 
 use pocketmine\player;
 use pocketmine\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\utils\Config;
+
+
 
 class NewPlugin extends listner{
 
 public function onEnable(){
-
-
-
-
-
+   $this->getServer()->getPluginManager()->registerEvents($this,$this);
+			$this->api = EconomyAPI::getInstance();
 }
-$pl() = array($coal, $iron, $gold, $diamond)
-give $pl(){
+{
+   $player = $event->getEntity();
+			$name = strtolower($player->getName());
+}
+if($player = $event->/lotto){
+   $this->api->reduceMoney($player, 100);
+   $plammount() = array($coal, $iron, $gold, $diamond);
+give $player{
    $coal rand(0,9);
   $iron rand(0,9);
   $gold rand(0,9);
@@ -30,12 +39,18 @@ $lotto = array($coal, $iron, $gold, $diamond){
  }
 }
 
-if($lotto = $pl){
+if($lotto = $plammount){
    echo "You've won the lotto!";
-   give $pl() $diamond;
+   $this->api->setMoney($player, rand(10, 20, 50, 100, 200));
 }
 else{
    echo "Sorry you lost play again?"
 }
-break;
+else{
+   
+}
+
+
+
+
 
